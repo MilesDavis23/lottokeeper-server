aa1558f536752bd8eb6180cf7c975a6b13b9b538
Miles Davis <foldes@mail.com>
2023-11-18 00:09:30 +0100

    sql->sql2, dal, checkuser update

--- db/database.js
+++ db/database.js
@@ -1,16 +1,23 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
     user: 'root',
     password: 'petiland',
-    database: 'CaselinkDB'
+    database: 'lottokeeper'
 });
 
-connection.connect((err) => {
-    if (err) throw err;
+const testConnection = async () => {
+  try {
+    const connection = await pool.getConnection();
     console.log('Connected to lottokeeper database.');
-  });
+    connection.release();
+  } catch (err) {
+    console.error('Database connection error:', err);
+  }
+}
+
+testConnection();
 
 module.exports = pool;
